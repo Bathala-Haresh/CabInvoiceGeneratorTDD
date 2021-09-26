1bb5741686d45bd74c521adc38ec993f312fc65a
Bathala-Haresh <hareshbathala87@gmail.com>
2021-09-26 14:50:18 +0530

    Added:Test cases to test result for multiple rides

--- src/test/java/example/CabInvoiceTests.java
+++ src/test/java/example/CabInvoiceTests.java
@@ -23,15 +23,15 @@ public class CabInvoiceTests {
         Assertions.assertEquals(5, result);
     }
 
-//    @Test
-//    public void givenMultipleRides_shouldReturnTotalAggregate() {
-//        CabInvoice cabInvoice = new CabInvoice();
-//        Ride[] rides = {
-//                new Ride(2.0, 5),
-//                new Ride(0.1, 1)
-//        };
-//        double fare = cabInvoice.calculateFare(rides);
-//        Assertions.assertEquals(30, fare);
-//    }
+    @Test
+    public void givenMultipleRides_shouldReturnTotalAggregate() {
+        CabInvoice cabInvoice = new CabInvoice();
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = cabInvoice.calculateFare(rides);
+        Assertions.assertEquals(30, fare);
+    }
 
 }
